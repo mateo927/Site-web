7aa9f93f54ebb30e85d5f8067b3a655cfd35e63e
mateo927 <mateocolllet07@icloud.com>
2026-03-01 16:30:01 +0100

    01/03

--- Mon_parcours/java/page_projet.js
+++ Mon_parcours/java/page_projet.js
@@ -3,15 +3,15 @@ function projets(Id){
     var text;
 
     if (Id=="ecole"){
-        text="Je compte continuer mes etudes a L'ENSIBS pour une 2 eme annee de prepa integree pour ensuite faire commencer mon cycle ingenieur en specialiter genie enregetique et electrique"
+        text="Je compte continuer mes études à l'ENSIBS en 2ème année de prépa intégrée, pour ensuite commencer mon cycle ingénieur en spécialité Génie Énergétique et Électrique."
     }
 
     if (Id=="bois"){
-        text="Je compte continuer le plus possible a faire des projets en bois pour le plaisir et pour apprendre de nouvelles techniques de travail pour l'instant je vais commencer a faire du mobilier pour travailler sur de plus grandes pieces.Mais aussi si  j'ai le temps me pencher sur la metelurgie pour perfectionner mes projets en bois ."
+        text="Je compte continuer le plus possible à faire des projets en bois, pour le plaisir et pour apprendre de nouvelles techniques. Pour l'instant, je vais commencer à faire du mobilier pour travailler sur de plus grandes pièces. Mais si j'ai le temps, je voudrais aussi me pencher sur la métallurgie pour perfectionner mes projets."
     }
 
     if (Id=="travail"){
-        text="Je cherche un stage pour l'été 2026 pour pouvoir rentrer dans le monde du travail plus precisement dans le domaine de l'energie et de l'electrique pour decouvrir ce domaine et pour valider mon choix de filiere de plus cela pourrat me faire une idee sur les entreprise possible pour ma future alternance en cycle ingenieur."
+        text="Je cherche un stage pour l'été 2026 pour pouvoir rentrer dans le monde du travail, plus précisément dans le domaine de l'énergie et de l'électrique. Cela me permettra de découvrir ce domaine, de valider mon choix de filière et de me faire une idée sur les entreprises possibles pour ma future alternance en cycle ingénieur."
     }
     document.getElementById("prochain-projet").textContent=text;
 }
